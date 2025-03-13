85e8d2f18e3c594050105258c07e36921d0e4dfb
mike <michael.guzman.personal@gmail.com>
2025-03-12 21:50:54 -0500

    installation working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use crate::cli::Cli;
 use crate::db::Manager;
 use clap::Parser;
+use std::env;
+use std::path::PathBuf;
 
 mod cli;
 mod db;
 mod error;
 fn main() {
-    let manager_res = Manager::new("/Users/mike/.local/share/db.sqlite");
+    let home_dir = env::var("HOME").unwrap_or_else(|_| ".".to_string());
+    let db_path = PathBuf::from(home_dir).join(".local/share/db.sqlite");
+
+    let manager_res = Manager::new(db_path.to_str().unwrap());
     let manager = manager_res.unwrap();
 
     let mut cli = Cli::parse();
